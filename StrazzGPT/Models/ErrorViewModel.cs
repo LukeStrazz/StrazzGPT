2b778bbe452b907ac501caef9821d1c0aa591cec
Luke <lukestrazzera@gmail.com>
2024-04-13 22:42:08 -0400

    Add project files.

---
+++ StrazzGPT/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace StrazzGPT.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
